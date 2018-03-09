12171429b797b66a161816885ff02aee5411d3f1
ggang.liu <304241378@qq.com>
2018-03-09 15:54:12 +0800

    Initial

---
+++ utils/LocalData.js
@@ -0,0 +1,19 @@
+const LocalData = {
+  isShowContact: true, //是否显示联系方式
+  baseInfo: { //基础信息
+    enName: '享受红利，刻不容缓', //英文名
+    title: '定制适合您的专属微信小程序',  //职位英文名
+    nameAndSex: '天府软件园职业程序员团队', 
+    birthday: '1111', //生日
+    diplomaAndSchool: '硕士 / 电子科技大学', //学历/学校
+    workAge: '7年', //工作年限
+    location: '成都',  //工作地点
+    wantPosition: '全栈工程师', //期望职位
+    mobilePhone: '18980460146',//移动手机
+    email: 'rider628@qq.com',//邮箱
+    wechatNo: 'ggang628',//微信号
+    github: '@ggangliu', //github 用户名
+    githubUrl: 'https://github.com/ggangliu', //github主页
+  }
+};
+module.exports = LocalData
